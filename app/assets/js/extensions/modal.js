5649ec50b66330c6c3ee938dd0df2a17e44f4fa1
Ivo Toman <ivorius@gmail.com>
2017-07-23 13:13:56 +0200

    customization product

--- app/assets/js/extensions/modal.js
+++ app/assets/js/extensions/modal.js
@@ -35,8 +35,30 @@
             $.colorbox({
                 href: el,
                 inline: true,
+                maxWidth: '95%',
+                maxHeight: '95%',
                 onOpen: function(){
                     el.fadeIn()
+                },
+                onComplete: function(){
+
+                  el.find("select[name='switch[zapinani]']").on('change', function(e) {
+                    if($(this).val() == 'visaci' || $(this).val() == 'patent') {
+                        el.find('.hidden_checkbox').show();
+                    } else {
+                        el.find('.hidden_checkbox').hide();
+                    }
+                  }).change();
+
+                  el.find("input[name='stone[prirodni_kamen]']").on('change', function() {
+                      var helper = $(this).parents('div.form-check').next('span.help-block');
+                     if(this.checked) {
+                        helper.show();
+                     } else {
+                         helper.hide();
+                     }
+                  }).change();
+
                 },
                 onCleanup: function(){
                     el.hide();
