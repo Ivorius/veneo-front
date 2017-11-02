2c765f99e522ee8b0aa91b8f0ef8dab140d488ef
Ivo Toman <ivorius@gmail.com>
2017-11-02 20:30:03 +0100

    call veneo functions after ajax

--- app/assets/js/extensions/loadProducts.js
+++ app/assets/js/extensions/loadProducts.js
@@ -1,14 +1,33 @@
 (function($, undefined) {
 
     $.nette.ext('ajaxContent', {
+        start: function() {
+            $.scrollupbar.destroy();
+        },
         success: function (payload) {
+
             if (payload.snippets) {
                 for (var i in payload.snippets) {
                     if(i == 'snippet--ajaxContent') {
                         recall();
+                        $("[data-header]").removeClass('o-header--fade').addClass('o-header--stay');
+
+                        veneoDropdown();
+                        veneoScroll();
+                        veneoInteractions();
+                        veneoToggle();
+                        veneoInit();
+                        veneoFlickity();
+                        veneoTabs();
+                        veneoMap();
+                        veneoWaypoints();
                     }
                 }
             }
+
+        },
+        complete: function() {
+            $("[data-header]").scrollupbar();
         }
     });
 
