f858d6fab2395335398c15d44c6c51a9b4d12aec
Ivo Toman <ivorius@gmail.com>
2017-12-22 10:41:31 +0100

    fix moving files

--- gulp/tasks/move.js
+++ gulp/tasks/move.js
@@ -2,7 +2,15 @@ var gulp = require("gulp"),
     config = require("../gulpconfig.json");
 
 
-gulp.task('move:files', ['build:compile'], function() {
+gulp.task('move:build', ['build'], function() {
+    console.log("Moving building files to folder /www/assets/");
+
+    gulp.src(config.dist.root + 'assets/**/*.*')
+        .pipe(gulp.dest('../www/assets/'));
+});
+
+
+gulp.task('move:files', function() {
     console.log("Moving files to folder /www/assets/");
 
     gulp.src(config.dist.root + 'assets/**/*.*')
